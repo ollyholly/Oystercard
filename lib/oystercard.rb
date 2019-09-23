4a5e4ccc70949ea9cf9fd1271166d2a4a4cf034d
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-09-23 16:04:20 +0100

    Max balance added to Oystercard, guard clause added to top_up.

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,11 +3,17 @@
 class Oystercard
   attr_accessor :balance
 
+  MAX_BALANCE = 90
+
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    if self.balance + amount > MAX_BALANCE
+      fail "Maximum balance is #{MAX_BALANCE}."
+    end
+    
     self.balance += amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,6 +4,7 @@ require 'oystercard'
 
 describe Oystercard do
   subject (:card) { described_class.new }
+  let (:max_balance) { Oystercard::MAX_BALANCE }
 
   describe '#balance' do
     it 'initialized with balance of 0' do
@@ -15,6 +16,11 @@ describe Oystercard do
     it 'adds money to the card' do
       card.top_up(10)
       expect(card.balance).to eq 10
+    end
+    it 'raises an error when trying to top up and balance goes over #{max_balance}' do
+      card.top_up(max_balance)
+      message = "Maximum balance is #{max_balance}."
+      expect{ card.top_up(1) }.to raise_error message
     end
   end
 end
