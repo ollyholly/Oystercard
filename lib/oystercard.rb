2acd032484ca7bb5136b793d7201d70b094fcf8f
Olly Holovchenko <oleh.holo@gmail.com>
2019-09-23 17:02:29 +0100

    #8 Add test and implementation for touch_in, touch_out and in_journey? methods

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 class Oystercard
-  attr_accessor :balance
+  attr_accessor :balance, :in_journey
 
   MAX_BALANCE = 90
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -14,11 +15,23 @@ class Oystercard
 
     self.balance += amount
   end
-  
+
   def deduct(amount)
     # raise "Maximum balance is #{MAX_BALANCE}." if over_maximum?(amount)
 
     self.balance -= amount
+  end
+
+  def touch_in
+    self.in_journey = true
+  end
+
+  def touch_out
+    self.in_journey = false
+  end
+
+  def in_journey?
+    in_journey
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,13 +3,19 @@
 require 'oystercard'
 
 describe Oystercard do
-  subject (:card) { described_class.new }
-  let (:max_balance) { Oystercard::MAX_BALANCE }
+  subject(:card) { described_class.new }
+  let(:max_balance) { Oystercard::MAX_BALANCE }
 
-  describe '#balance' do
+  describe '#initialize' do
+
     it 'initialized with balance of 0' do
       expect(card.balance).to eq 0
     end
+
+    it 'initializes not in journey' do
+      expect(card).not_to be_in_journey
+    end
+
   end
 
   describe '#top_up' do
@@ -30,4 +36,21 @@ describe Oystercard do
       expect { card.deduct(5) }.to change { card.balance }.by -5
     end
   end
+  
+  describe '#touch_in' do
+    
+    it 'sets the card to be in journey' do
+      card.touch_in
+      expect(card).to be_in_journey
+    end
+
+  end
+  
+  describe '#touch_out' do
+    it 'sets the card to be not in journey' do
+      card.touch_out
+      expect(card).not_to be_in_journey
+    end
+  end
+  
 end
