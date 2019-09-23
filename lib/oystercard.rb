5f066acad10baaed6d056fd61dcfc7e8564fd329
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-09-23 15:03:30 +0100

    Created test and feature for balance.

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,8 @@
 class Oystercard
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,9 @@ require 'oystercard'
 
 describe Oystercard do
 
+    describe '#balance' do
+      it 'initialized with balance of 0' do
+      expect(subject.balance).to eq 0
+      end
+    end
 end
